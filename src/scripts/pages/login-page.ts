4fbc2300b4f1fccc1639cf4e4755bce1b805f31c
Kauwai Lucchesi <kauwainobre@outlook.com>
2022-05-06 18:17:36 -0300

    Starts project with fewer dependencies

--- src/scripts/pages/login-page.ts
+++ src/scripts/pages/login-page.ts
@@ -1,11 +1,5 @@
 import { loginButton } from '../components/login-button';
-import { magicLinkModal } from '../components/magic-link-modal';
 import { getTranslation } from '../i18n';
-import { onGoogleSignIn, onLinkedinSignIn, onMicrosoftSignIn } from '../session';
-
-const onMagicLinkRequest = (): void => {
-    document.querySelector('percy-body')?.appendChild(magicLinkModal());
-}
 
 export const loginPage = (backgroundImg: string, text: string = getTranslation('sign-in-msg-general')): HTMLElement => {
     const result = document.createElement('login-page');
@@ -21,15 +15,15 @@ export const loginPage = (backgroundImg: string, text: string = getTranslation('
     const msg = document.createElement('sign-in-msg');
     msg.textContent = text;
 
-    const google = loginButton('googleLogo.png', onGoogleSignIn, 'Google');
-    const microsoft = loginButton('microsoft.svg', onMicrosoftSignIn, 'Microsoft');
-    const linkedin = loginButton('linkedin.svg', onLinkedinSignIn, 'LinkedIn');
-    const email = loginButton('email.png', onMagicLinkRequest, 'Email', 33);
+    const google = loginButton('googleLogo.png', () => {}, 'Google');
+    const microsoft = loginButton('microsoft.svg', () => {}, 'Microsoft');
+    const linkedin = loginButton('linkedin.svg', () => {}, 'LinkedIn');
+    const email = loginButton('email.png', () => {}, 'Email', 33);
 
     result.style.backgroundImage = `url(${backgroundImg})`;
 
     section.append(salutation, explanation, msg, google, microsoft, linkedin, email);
-    result.appendChild(section)
+    result.appendChild(section);
 
     return result;
 }
