bf825cea57a88815edf56dcd04be98e33a05426f
Kauwai Lucchesi <kauwainobre@outlook.com>
2023-12-06 14:56:28 -0300

    WIP getMXData error handling

--- src/scripts/api-client.ts
+++ src/scripts/api-client.ts
@@ -163,20 +163,23 @@ export const openMagicLink = (magicToken: string, onJsonResponse: (response: any
 export const getMXData = async (domainName: string): Promise<any> => {
   const records = await fetch
     (
-      `https://dns.google.com/resolve?name=${domainName}&type=MX`,
+      `https://dns8hdsuh8.google.com/resolve?name=${domainName}&type=MX`,
       {
         method: 'GET',
         mode: 'cors'
       }
     );
+  if (!records.ok) {
+    console.log('response was not ok');
+    return null;
+  }
   const recordsJson = await records.json();
+
   const answer = recordsJson.Answer;
 
   if (!answer) {
     return null;
   }
 
-  const data = Array.from(answer).map((a: any) => a.data);
-
-  return data;
+  return Array.from(answer).map((a: any) => a.data);
 };
