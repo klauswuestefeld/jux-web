8ecc83a42c28f74034ccd780a226748651e5327f
Kauwai Lucchesi <kauwainobre@outlook.com>
2024-02-05 16:53:23 -0300

    Simplify query logic

--- src/scripts/jux/query.ts
+++ src/scripts/jux/query.ts
@@ -1,25 +1,16 @@
-class QueryInit implements CustomEventInit {
-    bubbles?: boolean;
-    cancelable?: boolean;
-    composed?: boolean;
-    payload: any;
-    onResult: any;
-    onError: any;
-    
-    constructor(payload: any, onResult: any, onError?: any) {
-        this.payload = payload;
+class Query extends Event {
+    public params: any;
+    public onResult: (result: any) => void;
+    public onError?: (error: any) => void;
+
+    constructor(endpoint: string, onResult: (result: any) => void, params?: any, onError?: (error: any) => void) {
+        super(endpoint); // if needed, it is possible to modify bubbles, cancelable and composed properties
+        this.params = params;
         this.onResult = onResult;
         this.onError = onError;
     }
 }
 
-class Query<T> extends CustomEvent<T> {
-    constructor(endpoint: string, onResult: any, params?: any, onError?: any) {
-        const payload = new QueryInit(params, onResult, onError);
-        super(endpoint, payload);
-    }
-}
-
-export const query = (element: HTMLElement, endpoint: string, onResult: any, params?: any, onError?: any) => {
+export const query = (element: HTMLElement, endpoint: string, onResult: (result: any) => void, params?: any, onError?: (error: any) => void) => {
     element.dispatchEvent(new Query(endpoint, onResult, params, onError));
 }
